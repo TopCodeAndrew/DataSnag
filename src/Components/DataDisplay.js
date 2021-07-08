ce1919d7cef1fefb95b67c348ef10841e84e8649
Andrew Smith <topcodeandrew@gmail.com>
2021-07-08 17:13:59 -0600

    beginning data formatting

--- src/Components/DataDisplay.js
+++ src/Components/DataDisplay.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 
 let FULL_CONTACT_URL = "https://api.fullcontact.com/v3/person.enrich";
 
-console.log(FULL_CONTACT_URL);
+// console.log(FULL_CONTACT_URL);
 
 export default function DataDisplay(props) {
   const { identifierType, identifier } = props;
@@ -21,7 +21,7 @@ export default function DataDisplay(props) {
 
         axios.interceptors.request.use(
           (config) => {
-            console.log(bearerToken);
+            // console.log(bearerToken);
             config.headers.authorization = `Bearer ${bearerToken}`;
             return config;
           },
@@ -31,21 +31,122 @@ export default function DataDisplay(props) {
         );
 
         var jsonObj = {
-          email: "lloyd@lloydjsmith.com",
+          identifierTypeHolder: identifier,
         };
 
-        jsonObj.email = identifier;
+        jsonObj[identifierType] = jsonObj["identifierTypeHolder"];
+        delete jsonObj["identifierTypeHolder"];
 
         axios
           .post(`${FULL_CONTACT_URL}`, JSON.stringify(jsonObj))
           .then((res) => {
-            setUserData(Object.entries(res.data));
+            console.log(Object.entries(res.data["details"]));
+            setUserData(Object.entries(res.data["details"]));
           });
       })
       .catch((err) => console.log(err));
   }, []);
 
   let type = typeof userData;
+
+  let display = (data) => {
+    return <h2>{data}</h2>;
+  };
+
+  let mappedData =
+    // this ternary protects from an error while the user's data is being fetched by axios
+    type === "undefined"
+      ? null
+      : userData.map((e, i) => {
+          // console.log(e);
+          switch (e[0]) {
+            case "name":
+              return (
+                <div>
+                  <h1>NAME</h1>
+
+                  {Object.entries(e[1]).map((e) => {
+                    return (
+                      <div>
+                        <h3>
+                          {e[0]} : {e[1]}
+                        </h3>
+                      </div>
+                    );
+                  })}
+
+                  <br></br>
+                  <br></br>
+                  <br></br>
+                </div>
+              );
+              break;
+            case "age":
+              return (
+                <div>
+                  <h1>age</h1>
+                  {Object.entries(e[1]).map((e) => {
+                    return (
+                      <div>
+                        <h3>
+                          {e[0]} : {e[1]}
+                        </h3>
+                      </div>
+                    );
+                  })}
+
+                  <br></br>
+                  <br></br>
+                  <br></br>
+                </div>
+              );
+              break;
+            case "gender":
+              return (
+                <div>
+                  <h1>gender</h1>
+                  <h3>{e[1]}</h3>
+
+                  <br></br>
+                  <br></br>
+                  <br></br>
+                </div>
+              );
+              break;
+            case "demographics":
+              return <h1>demographics</h1>;
+              break;
+            case "emails":
+              return <h1>emails</h1>;
+              break;
+            case "phones":
+              return <h1>phones</h1>;
+              break;
+            case "profiles":
+              return <h1>profiles</h1>;
+              break;
+            case "locations":
+              return <h1>locations</h1>;
+              break;
+            case "employment":
+              return <h1>employment</h1>;
+              break;
+            case "photos":
+              return <h1>photos</h1>;
+              break;
+            case "education":
+              return <h1>education</h1>;
+              break;
+            case "urls":
+              return <h1>urls</h1>;
+              break;
+            case "interests":
+              return <h1>interests</h1>;
+              break;
+            default:
+              return <h2>other</h2>;
+          }
+        });
 
   let mappedData =
     type === "undefined"
@@ -57,7 +158,7 @@ export default function DataDisplay(props) {
               <h1>{i + 1}</h1>
               <h4>{e[0]}</h4>
               <h6>
-                {e[1]} <br></br>
+                {e[1] === null ? <h6> NULL</h6> : <h6>{e[1]}</h6>} <br></br>
                 <br></br>
               </h6>
             </div>
