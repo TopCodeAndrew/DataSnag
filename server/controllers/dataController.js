070bcde982cd051ec5e89bf405a7bc5443dbf414
Andrew Smith <topcodeandrew@gmail.com>
2021-07-14 16:26:13 -0600

    customizable architecture in back end in place

--- server/controllers/dataController.js
+++ server/controllers/dataController.js
@@ -5,13 +5,30 @@ const { FULL_CONTACT_BEARER_TOKEN } = process.env;
 
 module.exports = {
   getUserData: async (req, res) => {
-    const { url } = req.body;
+    const { url, identifierType, identifier } = req.body;
+
+    let reqBody = {};
+
+    if (identifierType === "email") {
+      reqBody = { email: identifier };
+    } else if (identifierType === "phone") {
+      reqBody = { phone: identifier };
+    }
+
     const returnedResult = await new Promise((resolve, reject) => {
       // this will probably simply need to be request.post (url......)
-      request(url, { json: true }, (err, res, body) => {
-        if (err) reject(err);
-        resolve(body);
-      });
+      request.post(
+        {
+          url: url,
+          json: true,
+          headers: { authorization: `Bearer ${FULL_CONTACT_BEARER_TOKEN}` },
+          body: reqBody,
+        },
+        (err, res, body) => {
+          if (err) reject(err);
+          resolve(body);
+        }
+      );
     });
 
     res.status(200).send(returnedResult);
